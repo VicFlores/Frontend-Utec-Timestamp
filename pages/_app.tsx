f036a46545e809640660af7a05c852738e198198
Vic Flores <vicsito2014@gmail.com>
2022-08-21 14:16:35 -0600

    feat(pages): global styles by styled components was added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+import '../styles/normalize.css';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Montserrat', sans-serif;
+    height: 100vh;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
