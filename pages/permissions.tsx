89b671d6ce21182cc3f58bc3ef306879aae2282d
Vic Flores <vicsito2014@gmail.com>
2022-08-26 16:50:12 -0600

    feat(pages): permissions pages was created

---
+++ pages/permissions.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Allowances from '../components/Allowances/Allowances';
+import Layout from '../components/Layout/Layout';
+
+const permissions = () => {
+  return <Allowances />;
+};
+
+export default permissions;
