2c04d0ef64359b1cad689ef6299889870c7254aa
Vic Flores <vicsito2014@gmail.com>
2022-09-04 12:14:31 -0600

    feat(components): login function was added

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 import {
   Button,
   LoginCardForm,
@@ -10,17 +13,65 @@ import {
   LoginContainer,
 } from './styles';
 
+type Inputs = {
+  email: string;
+  passwd: string;
+};
+
+type TErrorMessage = {
+  status: number;
+  message: string;
+};
+
 const Login = () => {
+  const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState<TErrorMessage>({
+    status: 0,
+    message: '',
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      await axios.post('/api/auth/login', data);
+
+      setErrorMessage({
+        status: 200,
+        message: 'Login Success',
+      });
+
+      router.push({
+        pathname: '/dashboard',
+      });
+    } catch (error: any) {
+      setErrorMessage(error.response.data);
+      return new Error(error.response.data);
+    }
+  };
+
   return (
     <LoginContainer>
       <LoginCardFormContainer>
         <LoginCardImageContainer>
           <LoginCardImage src='https://res.cloudinary.com/vicflores11/image/upload/v1661274853/frontend-utec-timestamp/undraw_login_re_4vu2_pcqgkc.svg' />
         </LoginCardImageContainer>
-        <LoginCardForm>
+        <LoginCardForm onSubmit={handleSubmit(onSubmit)}>
           <LoginCardFormTitle>Welcome to Utec Timestamp!</LoginCardFormTitle>
-          <LoginCardFormInput placeholder='Email' type='email' />
-          <LoginCardFormInput placeholder='Password' type='password' />
+          <LoginCardFormInput
+            {...register('email', { required: true })}
+            placeholder='Email'
+            type='email'
+          />
+          <LoginCardFormInput
+            {...register('passwd', { required: true })}
+            placeholder='Password'
+            type='password'
+          />
           <Button>Login</Button>
         </LoginCardForm>
       </LoginCardFormContainer>
