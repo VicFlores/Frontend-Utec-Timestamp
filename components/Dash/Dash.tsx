21d06343336f440add4bc93d4c991dfba317e252
Vic Flores <vicsito2014@gmail.com>
2022-10-06 11:44:50 -0600

    feat(compoents): Request to backend was added

--- components/Dash/Dash.tsx
+++ components/Dash/Dash.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import axios, { AxiosResponse } from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../context/AuthTokenContext';
+import { TArrayResponse } from '../../types/TArrayResponse';
 import Layout from '../Layout/Layout';
 import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from './styles';
 
 const Dash = () => {
+  const [allTimestamps, setAllTimestamps] = useState<TArrayResponse>();
+  const { authCookie }: any = useContext(AuthContext);
+
+  useEffect(() => {
+    try {
+      if (authCookie?.data) {
+        const getTimestamps = async () => {
+          const getTimestamps: any = await axios.get(
+            'https://backend-utec-timestamp.herokuapp.com/api/v1/timestamp',
+            {
+              headers: {
+                token: authCookie.data,
+                role: 'admin',
+              },
+
+              params: {
+                limit: 10,
+                skip: 0,
+              },
+            }
+          );
+
+          setAllTimestamps(getTimestamps);
+        };
+
+        getTimestamps();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [authCookie]);
+
   return (
     <Layout>
       <TableContainer>
@@ -16,18 +51,25 @@ const Dash = () => {
             </Tr>
           </Thead>
           <Tbody>
-            <Tr>
-              <Td>Vanessa Michelle</Td>
-              <Td>Melendez Escobar</Td>
-              <Td>Saturday, August 6th 2022, 10:20:11 am</Td>
-              <Td>Normal</Td>
-            </Tr>
-            <Tr>
-              <Td>Vic Ferman</Td>
-              <Td>Flores Escobar</Td>
-              <Td>Saturday, August 6th 2022, 10:20:11 am</Td>
-              <Td>Normal</Td>
-            </Tr>
+            {allTimestamps?.data ? (
+              allTimestamps.data.map((value: any) => {
+                return (
+                  <Tr key={value._id}>
+                    <Td>{value.result.name}</Td>
+                    <Td>{value.result.lastname}</Td>
+                    <Td>{value.dialing}</Td>
+                    <Td>{value.type}</Td>
+                  </Tr>
+                );
+              })
+            ) : (
+              <Tr key={Math.floor(Math.random() * 5) + 1}>
+                <Td>Waiting name</Td>
+                <Td>Waiting lastname</Td>
+                <Td>Waiting dialing</Td>
+                <Td>Waiting type of permissions</Td>
+              </Tr>
+            )}
           </Tbody>
         </Table>
       </TableContainer>
