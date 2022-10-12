72404c82d66b7adca99be982d8dfe265c0a2b89b
Vic Flores <vicsito2014@gmail.com>
2022-10-12 17:18:14 -0600

    feat(pages): authToken was added, also ssr authtoken was added

--- pages/private/userNotes/[_id].tsx
+++ pages/private/userNotes/[_id].tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
 import Notes from '../../../components/Notes/Notes';
+import { getCookie } from 'cookies-next';
+import { InferGetServerSidePropsType } from 'next';
+import SpecificsPermissions from '../../../components/SpecificsPermissions/SpecificsPermissions';
 
-const userNotes = () => {
-  return <Notes />;
+export const getServerSideProps = ({ req, res }: any) => {
+  const token = getCookie('MyTokenCookie', { req, res });
+
+  return { props: { token } };
+};
+
+const userNotes = ({
+  token,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return <SpecificsPermissions authToken={token} />;
 };
 
 export default userNotes;
