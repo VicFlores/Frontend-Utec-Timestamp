f62873734d34e17d760cc084eafe92167dc63575
Vic Flores <vicsito2014@gmail.com>
2022-10-14 12:21:33 -0600

    feat(pages): new user page was crated

--- pages/private/dashboard.tsx
+++ pages/private/dashboard.tsx
@@ -3,6 +3,9 @@ import Dash from '../../components/Dash/Dash';
 import { getCookie } from 'cookies-next';
 import jwt from 'jsonwebtoken';
 import { InferGetServerSidePropsType } from 'next';
+import { IPayload } from '../../types/TPayload';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
 export const getServerSideProps = ({ req, res }: any) => {
   const token: any = getCookie('MyTokenCookie', { req, res });
@@ -15,6 +18,20 @@ const dashboard = ({
   token,
   decodedToken,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+  const rolValidation = decodedToken as unknown as IPayload;
+
+  /* if (rolValidation.rol !== 'admin') {
+    const logout = async () => {
+      await axios.post('/api/auth/logout');
+      router.push({
+        pathname: '/login',
+      });
+    };
+
+    logout();
+  } */
+
   return <Dash authToken={token} decoded={decodedToken} />;
 };
 
